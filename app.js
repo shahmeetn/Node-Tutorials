19fa329b5d21aaf844d30aee68a3620349d693ee
shahmeetn <meetshah7256@gmail.com>
2017-08-14 17:08:47 +0530

    base structure of project

---
+++ app.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+const config = require('./config/database');
+
+mongoose.connect(config.database);
+
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', function () {
+    console.log('Connected to MongoDB');
+});
+
+// Check for DB errors
+db.on('error', function (err) {
+    console.log(err);
+});
+
+// Init App
+const app = express();
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+// parse application/json
+app.use(bodyParser.json());
+
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Express Session Middleware
+app.use(session({
+    secret: 'mSNZjn,8e6#~*M\M',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.'),
+            root = namespace.shift(),
+            formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+
+// Passport Config
+require('./config/passport')(passport);
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('*', function (req, res, next) {
+    res.locals.user = req.user || null;
+    next();
+});
+
+// Home Route
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+// Route Files
+let dashboard = require('./routes/dashboard');
+app.use('/dashboard', dashboard);
+
+// Start Server
+app.listen(3000, function () {
+    console.log('Server started on port 3000...');
+});
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database: 'mongodb://localhost:27017/s3',
+    secret: 'L&d_,EkW5#k4_U&W'
+}
